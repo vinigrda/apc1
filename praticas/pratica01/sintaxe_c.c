5a26975297844594be0bb811fdc097723e147efe
vinigrda <vinicius.gabriel250303@gmail.com>
2024-08-15 00:48:17 +0000

    condigos da pratica01

---
+++ praticas/pratica01/sintaxe_c.c
@@ -0,0 +1,26 @@
+#include <stdio.h> // inclusao de bibliotecas
+
+int main() { // funcao principal
+  // seu codigo aqui
+
+  // 1. entrada
+  // levanter meus ingredientes
+  // char, int, float
+  // variaveis
+  // leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+    
+  
+  // 2. processamento
+  // calculos soma, subtracao, multiplicacao, divisao
+  int soma = primeiro_numero + segundo_numero;
+
+  // 3. saida
+  // escrita
+  printf("A soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+  
+  return 0; // retorna fim do programa
+}
